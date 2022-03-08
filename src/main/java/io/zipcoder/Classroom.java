ef8c57397b97f7b2e337d0e1e19fd1eb86172e2d
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-03-08 17:50:35 -0500

    completed 2.2

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
 public class Classroom {
+    public Student[] students;
+    public int maxNumberOfStudents;
+
+    public Classroom(int maxNumberOfStudents){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+// students = new Student[maxNumberOfStudents]; --- this is functionally the same as ^^^, just written a
+// different way
+    }
+
+    public Classroom(Student[] student){
+        students = student;
+    }
+
+    public Classroom(){
+        students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
 }
