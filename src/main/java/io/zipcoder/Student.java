3536aedaccecd80298f06fea2775880ece96ca1e
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-03-08 16:03:51 -0500

    completed 1.3

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,41 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    public String firstName;
+    public String lastName;
+    public ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(examScores));
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
 }
