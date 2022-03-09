734d4bb7292dfdf8a7bee7e95c74ccd9d0a671d0
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-03-09 00:00:53 -0500

    started to set up tests

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class ClassroomTest {
+    // Ensure positive and negative unit tests exist per feature of the application.
+    // https://studiofreya.com/java/testing-java-with-junit/
+    // A positive test is a test that verifies a correct functionality, a negative test verifies that
+    // a function will fail or throw an exception.
+
+    @Test
+    // Given
+    // When
+    // Then
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,35 @@
 package io.zipcoder;
 
-public class StudentTest {
+import org.junit.Test;
 
+public class StudentTest {
+    // Ensure positive and negative unit tests exist per feature of the application.
+    // https://studiofreya.com/java/testing-java-with-junit/
+    // A positive test is a test that verifies a correct functionality, a negative test verifies that
+    // a function will fail or throw an exception.
+
+    @Test
+    // Given
+    // When
+    // Then
+
+    @Test
+    // Given
+    // When
+    // Then
+
+    @Test
+    // Given
+    // When
+    // Then
+
+    @Test
+    // Given
+    // When
+    // Then
+
+    @Test
+    // Given
+    // When
+    // Then
 }
